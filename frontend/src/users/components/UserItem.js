7ea9ff38fd3ece1f8d4de9f24785906e615c4fa3
Jakub Rzeszutko <kubarzeszutko@gmail.com>
2022-07-11 16:02:26 +0200

    Add strucutre for the front end and user and place pages with routing

---
+++ frontend/src/users/components/UserItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './UserItem.css'
+
+const UserItem = () => {
+    return (
+        <div>UserItem</div>
+    )
+}
+
+export default UserItem
---
+++ frontend/src/users/components/UsersList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './UserList.css'
+
+const UsersList = () => {
+    return (
+        <div>UsersList</div>
+    )
+}
+
+export default UsersList
