64dff4ce3b649c6788e837afce8a5f36fffa550e
Jakub Rzeszutko <kubarzeszutko@gmail.com>
2022-07-13 20:10:16 +0200

    Add place list and place item components with place page

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -3,8 +3,13 @@ import React from 'react'
 import './PlaceItem.css'
 
 const PlaceItem = () => {
+
+    if (props.places.length === 0) {
+        return (
+            <p>This user has no places added...</p>
+        )
+    }
     return (
-        <div>PlaceItem</div>
     )
 }
 
--- frontend/src/places/components/PlaceList.js
+++ frontend/src/places/components/PlaceList.js
@@ -2,9 +2,25 @@ import React from 'react'
 
 import './PlaceList.css'
 
+import Card from '../../shared/components/UIElements/Card'
+import PlaceItem from './PlaceItem'
+
 const PlaceList = () => {
+
+    if (props.places.length === 0) {
+        return (
+            <div className='place-list center'>
+                <Card>
+                    <h2>This user hsa not added any places...</h2>
+                </Card>
+            </div>)
+    }
     return (
-        <div>PlaceList</div>
+        <ul className='place-list'>
+            {props.places.map((place) => {
+                <PlaceItem />
+            })}
+        </ul>
     )
 }
 
