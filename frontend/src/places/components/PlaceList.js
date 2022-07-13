405ef341c73201b26f142cf935bb75a8e967c276
Jakub Rzeszutko <kubarzeszutko@gmail.com>
2022-07-13 20:57:49 +0200

    Add a user places route and page which filters the places which belong to a user

--- frontend/src/places/components/PlaceList.js
+++ frontend/src/places/components/PlaceList.js
@@ -5,21 +5,27 @@ import './PlaceList.css'
 import Card from '../../shared/components/UIElements/Card'
 import PlaceItem from './PlaceItem'
 
-const PlaceList = () => {
+const PlaceList = (props) => {
 
     if (props.places.length === 0) {
         return (
             <div className='place-list center'>
                 <Card>
-                    <h2>This user hsa not added any places...</h2>
+                    <h2>This user has not added any places...</h2>
                 </Card>
             </div>)
     }
     return (
         <ul className='place-list'>
-            {props.places.map((place) => {
-                <PlaceItem />
-            })}
+            {props.places.map((place) => <PlaceItem
+                key={place.id}
+                id={place.id}
+                image={place.image}
+                title={place.title}
+                description={place.description}
+                address={place.address}
+                creator={place.creator}
+                coordinates={place.coordinates} />)}
         </ul>
     )
 }
--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -1,9 +1,52 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+
+import PlaceList from '../components/PlaceList'
 
 const UserPlaces = () => {
-  return (
-    <div>UserPlaces</div>
-  )
+
+  const DUMMY_PLACES = [{
+    id: 1,
+    image: 'https://media.istockphoto.com/photos/aerial-view-of-the-city-bar-picture-id1132779096?k=20&m=1132779096&s=612x612&w=0&h=C_9iFigKra6NyhTwCNpyjYvk6yhZ6O5c-9a0c9c0H3Q=',
+    title: 'Croatia Bar',
+    description: 'Very clear water in the sea',
+    address: 'Montenegro bar',
+    creator: 1,
+    coordinates: {
+      lat: 42.1021263,
+      lng: 19.0706027
+    }
+  },
+  {
+    id: 2,
+    image: 'https://media.istockphoto.com/photos/aerial-view-of-the-city-bar-picture-id1132779096?k=20&m=1132779096&s=612x612&w=0&h=C_9iFigKra6NyhTwCNpyjYvk6yhZ6O5c-9a0c9c0H3Q=',
+    title: 'Croatia Bar',
+    description: 'Very clear water in the sea',
+    address: 'Montenegro bar',
+    creator: 2,
+    coordinates: {
+      lat: 42.1021263,
+      lng: 19.0706027
+    }
+  },
+  {
+    id: 3,
+    image: 'https://media.istockphoto.com/photos/aerial-view-of-the-city-bar-picture-id1132779096?k=20&m=1132779096&s=612x612&w=0&h=C_9iFigKra6NyhTwCNpyjYvk6yhZ6O5c-9a0c9c0H3Q=',
+    title: 'Croatia Bar',
+    description: 'Very clear water in the sea',
+    address: 'Montenegro bar',
+    creator: 3,
+    coordinates: {
+      lat: 42.1021263,
+      lng: 19.0706027
+    }
+  }]
+
+  const userId = useParams().userId
+
+  const userPlaces = DUMMY_PLACES.filter((place) => place.creator == userId)
+
+  return <PlaceList places={userPlaces} />
 }
 
 export default UserPlaces
